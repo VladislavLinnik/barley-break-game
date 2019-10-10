d49f4870c373857d7884cda02349b0ec36e4a73f
Vladislav <vlad.l@houstonapps.co>
2019-10-10 15:35:58 +0300

    Game was initialized

---
+++ script.js
@@ -0,0 +1,71 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const game = document.querySelector('.game');
+const result = [];
+let counter;
+
+numbers = shuffleArray(numbers);
+
+
+initializeGame();
+document.querySelector('.js-startBtn').addEventListener('click', initializeGame);
+
+                                                        
+document.onclick = function(event) {
+  event.preventDefault();
+  let currentNumber = event.target.dataset.number;
+  
+  if (!event.target.dataset.hasOwnProperty('number')) return false;
+  
+  if (~result.indexOf(currentNumber)) return false; // If element already exist (~ is equal to != -1)
+  
+  if (counter !== +currentNumber) return false;
+  counter++;
+  result.push(currentNumber);
+  event.target.classList.add('active');
+  
+  if (result.length === numbers.length) finishGame();
+}
+                                                        
+                                                        
+function initializeGame() {
+  game.innerHTML = null;
+  counter = 1;
+  result.length = 0;
+  numbers = shuffleArray(numbers);
+  createGrid();
+}
+
+function finishGame() {
+  alert('Game has been finished');
+  initializeGame();
+}
+
+function createGrid() {
+  const grid = document.createElement('div');
+  grid.classList.add('grid');
+  
+  numbers.forEach((cell) => {
+    const gridCell = document.createElement('div');
+    gridCell.classList.add('grid-cell');
+    gridCell.dataset.number = cell;
+    gridCell.appendChild(document.createTextNode(cell));
+    grid.appendChild(gridCell);
+  });
+  
+  
+  game.appendChild(grid);
+}
+
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
